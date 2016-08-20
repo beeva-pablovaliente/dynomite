fe9e7772ee4da00b3b29429dd8588a76eb9ef953
Juan Manuel Perez Rodriguez <jm.perez.rodriguez@bbva.com>
2016-08-20 11:17:56 +0200

    Modified florida server to read seed.list from S3 bucket

--- scripts/Florida/florida.js
+++ scripts/Florida/florida.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var AWS = require('aws-sdk');
+var bytes = require('bytes');
 
 // Settings
 var port = process.env.DYNOMITE_FLORIDA_PORT ?
@@ -20,16 +22,51 @@ http.createServer(function(req, res) {
 
   res.writeHead(200, {'Content-Type': 'application/json'});
   if (path === apiUrl) {
-    fs.readFile(seedsFilePath, 'utf-8', function(err, data) {
-      if (err) console.log(err); 
+    if (process.env.DYNOMITE_S3_SEED_FILE) {
+        console.log("Retrieving seed file from S3");
 
-      var now = (new Date()).toJSON();
-      var seeds = data.trim().replace(/\n/g, '|');
+        // We assume that credentials are available through IAM Role
+        // Otherwise, should be configured properly locally (http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html)
+        AWS.config.update({
+            region: 'eu-west-1'
+        );
 
-      enableDebug && console.log(now + ' - get_seeds [' + seeds + ']');
-      res.write(seeds);
-      res.end();
-    });
+        // Seed file retrieved from S3
+        var s3 = new AWS.S3();
+
+        var params = {
+          Bucket: 'poc-dynomite-backups',
+          Key: 'seeds.list',
+          ResponseContentType: 'text/plain'
+        };
+
+        s3.getObject(params, function(err, data) {
+          if (err) {
+            console.log(err, err.stack);
+          } else {
+            console.log(data.Body.toString('utf-8'));
+            var fileContent = data.Body.toString('utf-8');
+
+            var now = (new Date()).toJSON();
+            var seeds = fileContent.trim().replace(/\n/g, '|');
+
+            enableDebug && console.log(now + ' - get_seeds [' + seeds + ']');
+            res.write(seeds);
+            res.end();
+          }
+        });
+    } else {
+        fs.readFile(seedsFilePath, 'utf-8', function(err, data) {
+          if (err) console.log(err);
+
+          var now = (new Date()).toJSON();
+          var seeds = data.trim().replace(/\n/g, '|');
+
+          enableDebug && console.log(now + ' - get_seeds [' + seeds + ']');
+          res.write(seeds);
+          res.end();
+        });
+    }
   } else {
     res.end();
   }
