1cfd989200777edab0b7283c1326efc55b48cef7
Pablo Valiente <pabloantonio.valiente@beeva.com>
2016-08-28 20:31:19 +0200

    Delete unused module

--- scripts/Florida/florida.js
+++ scripts/Florida/florida.js
@@ -2,7 +2,6 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var AWS = require('aws-sdk');
-var bytes = require('bytes');
 
 // Settings
 var port = process.env.DYNOMITE_FLORIDA_PORT ?
